537e08c4f92287c5f3fd9e3eced1c1d7aca97cbc
David Antonio <happywybie@gmail.com>
2024-09-18 16:51:19 -0600

    test(terms): add term file

--- app/src/main/java/com/firstgroup/cmisentinelprimemobile/terms.kt
+++ app/src/main/java/com/firstgroup/cmisentinelprimemobile/terms.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class terms : AppCompatActivity() {
+class Terms : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
