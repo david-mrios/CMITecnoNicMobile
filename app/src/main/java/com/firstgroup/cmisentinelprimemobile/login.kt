06f27de4ad0cbdeac17d6317c5941c040a1d416a
David Antonio <happywybie@gmail.com>
2024-09-18 18:30:41 -0600

    ci(login): add logic to navigate to a new activity on successful login

--- app/src/main/java/com/firstgroup/cmisentinelprimemobile/login.kt
+++ app/src/main/java/com/firstgroup/cmisentinelprimemobile/login.kt
@@ -1,20 +1,75 @@
 package com.firstgroup.cmisentinelprimemobile
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Patterns
+import android.content.Intent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.firstgroup.cmisentinelprimemobile.databinding.ActivityLoginBinding
 
 class login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private val fixedPassword = "123456"
+    private val fixedEmail = "happywybie@gmail.com"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.editTxtEmail.text.toString()
+            val password = binding.editTextPass.text.toString().trim()
+
+            if (validateEmail(email) && validatePassword(password)) {
+                if (password == fixedPassword && fixedEmail == email ) {
+                    val intent = Intent(this, MainMenu::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Correo o contraseña inválidos.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.linkSingUp.setOnClickListener {
+            val intent = Intent(this, register::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return if (email.isEmpty()) {
+            binding.editTxtEmail.error = "El correo no puede estar vacío"
+            false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.editTxtEmail.error = "Correo inválido"
+            false
+        } else {
+            binding.editTxtEmail.error = null
+            true
+        }
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        return if (password.isEmpty()) {
+            binding.editTextPass.error = "La contraseña no puede estar vacía"
+            false
+        } else {
+            binding.inputPass.error = null
+            true
         }
     }
 }
