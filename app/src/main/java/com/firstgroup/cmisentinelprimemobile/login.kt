026d9b92efe0b740fc7ceec2b488b5398736aa93
David Antonio <happywybie@gmail.com>
2024-10-10 21:22:30 -0600

    ci(login): remove email and password validation for direct navigation

--- app/src/main/java/com/firstgroup/cmisentinelprimemobile/login.kt
+++ app/src/main/java/com/firstgroup/cmisentinelprimemobile/login.kt
@@ -1,9 +1,7 @@
 package com.firstgroup.cmisentinelprimemobile
 
 import android.os.Bundle
-import android.util.Patterns
 import android.content.Intent
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,8 +9,6 @@ import com.firstgroup.cmisentinelprimemobile.databinding.ActivityLoginBinding
 
 class login : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private val fixedPassword = "123456"
-    private val fixedEmail = "happywybie@gmail.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,43 +23,9 @@ class login : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener {
-            val email = binding.editTxtEmail.text.toString()
-            val password = binding.editTextPass.text.toString().trim()
-
-            if (validateEmail(email) && validatePassword(password)) {
-                if (password == fixedPassword && fixedEmail == email ) {
-                    val intent = Intent(this, MainMenu::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Correo o contraseña inválidos.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun validateEmail(email: String): Boolean {
-        return if (email.isEmpty()) {
-            binding.editTxtEmail.error = "El correo no puede estar vacío"
-            false
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            binding.editTxtEmail.error = "Correo inválido"
-            false
-        } else {
-            binding.editTxtEmail.error = null
-            true
-        }
-    }
-
-    private fun validatePassword(password: String): Boolean {
-        return if (password.isEmpty()) {
-            binding.editTextPass.error = "La contraseña no puede estar vacía"
-            false
-        } else {
-            binding.inputPass.error = null
-            true
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
